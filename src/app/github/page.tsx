331974087e544345b74067a26e2e844b8ea6fffd
lucanevess87 <lucatets@hotmail.com>
2024-01-01 16:43:59 -0300

    add github page

---
+++ src/app/github/page.tsx
@@ -0,0 +1,20 @@
+import { Fragment } from 'react';
+
+import { Detail } from '@/components/Detail';
+import { MainContent } from '@/components/MainContent';
+import { ResizableHandle, ResizablePanel } from '@/components/ui/resizable';
+
+export default function GithubPage() {
+  return (
+    <Fragment>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={45}>
+        <MainContent />
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={35}>
+        <Detail />
+      </ResizablePanel>
+    </Fragment>
+  );
+}
